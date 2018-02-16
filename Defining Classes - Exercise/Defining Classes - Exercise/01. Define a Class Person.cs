686f8ca6e39c71f36b6fa07487d242711b099e74
giggly <giggly@abv.bg>
2018-02-17 00:00:17 +0200

    Defining Classes - Exercise

---
+++ Defining Classes - Exercise/Defining Classes - Exercise/01. Define a Class Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defining_Classes___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
