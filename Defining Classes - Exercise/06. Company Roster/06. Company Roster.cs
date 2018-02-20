06ce1aa806332d580c9992579e7fedb9b0af8ed5
giggly <giggly@abv.bg>
2018-02-20 23:11:34 +0200

    Company Roster

---
+++ Defining Classes - Exercise/06. Company Roster/06. Company Roster.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Company_Roster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<Employee> employees = new List<Employee>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] emp = Console.ReadLine()
+                                .Split()
+                                .ToArray();
+                if (emp.Count() == 6)
+                {
+                    Employee newEmp = new Employee(emp[0], double.Parse(emp[1]), emp[2], emp[3], emp[4], int.Parse(emp[5]));
+                    employees.Add(newEmp);
+                }
+                else if (emp.Count() == 5)
+                {
+                    Employee newEmp = new Employee(emp[0], double.Parse(emp[1]), emp[2], emp[3], emp[4]);
+                    employees.Add(newEmp);   
+                }
+                else if (emp.Count() == 4)
+                {
+                    Employee newEmp = new Employee(emp[0], double.Parse(emp[1]), emp[2], emp[3]);
+                    employees.Add(newEmp);
+                }
+
+
+            }
+
+            var highestAverageSalaryDepartment = employees
+                .GroupBy(e => e.department)
+                .OrderByDescending(g => g.Select(e => e.salary).Sum())
+                .First();
+
+            Console.WriteLine($"Highest Average Salary: {highestAverageSalaryDepartment.Key}");
+            Console.WriteLine(string.Join(Environment.NewLine, highestAverageSalaryDepartment
+                .OrderByDescending(e => e.salary)
+                .Select(e => $"{e.name} {e.salary:F2} {e.email} {e.age}")));
+
+
+        }
+    }
+}
---
+++ Defining Classes - Exercise/06. Company Roster/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Company_Roster
+{
+    public class Employee
+    {
+        public string name { get; set; }
+        public double salary { get; set; }
+        public string position { get; set; }
+        public string department { get; set; }
+        public string email { get; set; }
+        public int age { get; set; }
+
+        public Employee (string name, double salary, string position, string department, string email="n/a", int age=-1)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.position = position;
+            this.department = department;
+            this.email = email;
+            this.age = age;
+        }
+
+    }
+}
