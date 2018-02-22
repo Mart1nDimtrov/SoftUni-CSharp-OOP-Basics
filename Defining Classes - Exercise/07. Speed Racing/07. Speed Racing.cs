021c1c7765aa16fd81228e3617c5de52aa3befd3
giggly <giggly@abv.bg>
2018-02-22 12:03:53 +0200

    Speed Racing

---
+++ Defining Classes - Exercise/07. Speed Racing/07. Speed Racing.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Speed_Racing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] carInput = Console.ReadLine()
+                                        .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                        .ToArray();
+                Car newCar = new Car(carInput[0], double.Parse(carInput[1]), double.Parse(carInput[2]));
+                cars.Add(newCar);
+            }
+
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] commandSplit = command
+                                            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                            .ToArray();
+                Car car = cars.Where(c => c.model == commandSplit[1])
+                              .First();
+                car.CalculateDistanceTraveled(double.Parse(commandSplit[2]));
+              
+                command = Console.ReadLine();
+            }
+
+            cars.ToList()
+                .ForEach(c => Console.WriteLine($"{c.model} {c.fuelAmount:F2} {c.distanceTraveled}"));
+
+        }
+    }
+}
---
+++ Defining Classes - Exercise/07. Speed Racing/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Speed_Racing
+{
+    public class Car
+    {
+        public string model { get; set; }
+        public double fuelAmount { get; set; }
+        public double fuelConsumption { get; set; }
+        public double distanceTraveled { get; set; }
+
+        public Car (string model, double fuelAmount, double fuelConsumption)
+        {
+            this.model = model;
+            this.fuelAmount = fuelAmount;
+            this.fuelConsumption = fuelConsumption;
+            this.distanceTraveled = 0;
+        }
+
+        public void CalculateDistanceTraveled (double distance)
+        {
+            if (distance * this.fuelConsumption <= this.fuelAmount)
+            {
+                this.fuelAmount -= (distance * this.fuelConsumption);
+                this.distanceTraveled += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+    }
+}
