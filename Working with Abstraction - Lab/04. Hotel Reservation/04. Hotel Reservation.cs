741bda08ca37ad28381f204c0b4beaceba656516
giggly <giggly@abv.bg>
2018-05-12 23:12:11 +0300

    Hotel Reservation

---
+++ Working with Abstraction - Lab/04. Hotel Reservation/04. Hotel Reservation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Hotel_Reservation
+{
+    class Program
+    {
+        public enum Season { Autumn = 1, Spring = 2, Winter = 3, Summer = 4 };
+        static void Main(string[] args)
+
+        {
+        string[] hotelRes = Console.ReadLine()
+                                    .Split()
+                                    .ToArray();
+
+            if (hotelRes.Length == 3)
+            {
+                Console.WriteLine("{0:F2}", PriceCalculator.CalcultaPrice(decimal.Parse(hotelRes[0]),
+                    int.Parse(hotelRes[1]), (int)(Season)Enum.Parse(typeof(Season), hotelRes[2]))); 
+            }
+            else if (hotelRes.Length == 4)
+            {
+
+                Console.WriteLine("{0:F2}", PriceCalculator.CalcultaPrice(decimal.Parse(hotelRes[0]),
+                    int.Parse(hotelRes[1]), (int)(Season)Enum.Parse(typeof(Season), hotelRes[2]), hotelRes[3]));
+
+
+            }
+
+        }
+    }
+}
---
+++ Working with Abstraction - Lab/04. Hotel Reservation/PriceCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Hotel_Reservation
+{ 
+    public class PriceCalculator
+    {
+
+        public static decimal CalcultaPrice(decimal pPD, int numDays, int season)
+        {
+            return (pPD * season) * numDays;
+        }
+
+        public static decimal CalcultaPrice(decimal pPD, int numDays, int season, string discountType)
+        {
+            if (discountType == "VIP")
+            {
+                return ((pPD * season) * numDays) - (((pPD * season) * numDays) * 0.20m);
+            }
+            if (discountType == "SecondVisit")
+            {
+                return ((pPD * season) * numDays) - (((pPD * season) * numDays) * 0.10m);
+            }
+            return (pPD * season) * numDays;
+
+        }
+
+    }
+}
